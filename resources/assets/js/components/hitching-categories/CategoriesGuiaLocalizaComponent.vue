4e6fc1ebc5a18c681be2fda64202d8e15135c350
williamsduarte <wsduarte@outlook.com>
2018-04-02 08:41:16 -0400

    Atualizar combo

--- resources/assets/js/components/hitching-categories/CategoriesGuiaLocalizaComponent.vue
+++ resources/assets/js/components/hitching-categories/CategoriesGuiaLocalizaComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="form-group">
     <label>Selecione a categoria do GuiaLocaliza</label>
+    <button v-on:click.stop.prevent="updateCombo">Atualizar Combo</button>
     <select class="form-control select2" required name="category_gl" style="width: 100%;">
       <option selected="selected" value="">Selecione</option>
       <option v-for="option in options" v-bind:value="option.id">
@@ -18,7 +19,28 @@
         options : []
       }
     },
-    created() {
+    methods: {
+
+      updateCombo: function (event) {
+
+
+        let self = this;
+        self.options = [];
+
+        axios.get('/get/categories')
+          .then(function (response) {
+            self.options = response.data;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+
+
+      }      
+        
+    },
+    created(){
 
       let self = this;
 
@@ -29,7 +51,7 @@
         .catch(function (error) {
           console.log(error);
         });
-        
+
     },
     mounted() {
 
